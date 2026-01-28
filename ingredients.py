02c16fcfc0263cebfbd779b6d15c6d36400ac72b
Alice <as537332@georgetownisd.org>
2026-01-28 09:49:22 -0600

    Update ingredients.py

--- ingredients.py
+++ ingredients.py
@@ -1,39 +1,40 @@
-mony=200
+money=200
 lemon=0
 cup=0
 ice=0
-shuger=0
+sugar=0
 
 def add(num,num1):
     number= num+num1
     return number
 
-buy=int(input("1 =lemon 2=cup 3=shuger 4=ice"))
+buy=int(input("1 =lemon 2=cup 3=sugar 4=ice"))
 buyness=int(input("how much you whant to buy"))
 if buyness <= 0:
-    print("you diry")
+    print("please put in what you want to buy.")
 elif buy==1:
     if mony<=buyness*-2+mony:
-        print("your to poor")
+        print("you don't have enough.")
     else:
-        mony=buyness*-2+mony
+        mony=buyness*-2+money
         lemon=buyness
 elif buy==2:
-    if mony<=buyness*-1+mony:
-        print("your to poor")
+    if money<=buyness*-1+money:
+        print("you don't have enough.")
     else:
-        mony=buyness*-1+mony
+        money=buyness*-1+money
         cupn=buyness
 elif buy==3:
-    if mony<=buyness*-3+mony:
+    if money<=buyness*-3+mony:
         print("your to poor")
     else:
-        mony=buyness*-3+mony
+        money=buyness*-3+money
         shuger=buyness
 elif buy==4:
-    if mony<=buyness*-2+mony:
+    if money<=buyness*-2+money:
         print("your to poor")
     else:
-        mony=buyness*-1+mony
+        money=buyness*-1+money
         ice=buyness
-print (mony)
+print (money)
+
