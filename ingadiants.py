2b35fc96807719407a7a65cad69119d86457ea5a
Alice <as537332@georgetownisd.org>
2026-01-29 09:44:46 -0600

    Update ingadiants.py

--- ingadiants.py
+++ ingadiants.py
@@ -1,27 +1,28 @@
-mony=200
+money=200
 lemon=0
 cup=0
 ice=0
-shuger=0
+sugar=0
 
 def add(num,num1):
     number= num+num1
     return number
 
-buy=int(input("1 =lemon 2=cup 3=shuger 4=ice"))
+buy=int(input("1 =lemon 2=cup 3=sugar 4=ice"))
 buyness=int(input("how much you whant to buy"))
 if buyness <= 0:
-    print("you diry")
+    print("please try again.")
 elif buy==1:
-    mony=buyness*-2+mony
+    money=buyness*-2+money
     lemon=buyness
 elif buy==2:
-    mony=buyness*-1+mony
+    money=buyness*-1+money
     cup=buyness
 elif buy==3:
-    mony=buyness*-3+mony
-    shuger=buyness
+    money=buyness*-3+money
+    sugar=buyness
 elif buy==4:
-    mony=buyness*-20+mony
+    money=buyness*-20+money
     ice=buyness
-print (mony)
+
+print (money)
