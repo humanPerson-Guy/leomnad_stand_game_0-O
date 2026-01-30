cb6adc8b1afc425249388d08eb8baa7dcc717201
Alice <as537332@georgetownisd.org>
2026-01-30 09:37:52 -0600

    Update ingrtants.py

--- ingrtants.py
+++ ingrtants.py
@@ -2,36 +2,37 @@ def add(num,num1):
         number= num+num1
         return number
 
-def shop(mony=100,lemon=10,cup=10,ice=10,shuger=10):
+def shop(money=100,lemon=10,cup=10,ice=10,sugar=10):
 
-    buy=int(input("1 =lemon 2=cup 3=shuger 4=ice"))
-    buyness=int(input("how much you whant to buy"))
+    buy=int(input("1 =lemon 2=cup 3=sugar 4=ice"))
+    buyness=int(input("how much do you whant to buy?"))
     if buyness <= 0:
-        print("you diry")
+        print("go back")
     elif buy==1:
-        if mony<=buyness*-2+mony:
-            print("your to poor")
+        if money<=buyness*-2+money:
+            print("you dont have enough.")
         else:
-            mony=buyness*-2+mony
+            money=buyness*-2+money
             lemon=buyness
     elif buy==2:
-        if mony<=buyness*-1+mony:
-            print("your to poor")
+        if money<=buyness*-1+money:
+            print("you dont have enough.")
         else:
-            mony=buyness*-1+mony
+            money=buyness*-1+money
             cup=buyness
     elif buy==3:
-        if mony<=buyness*-3+mony:
-            print("your to poor")
+        if money<=buyness*-3+money:
+            print("you don't have enough.")
         else:
-            mony=buyness*-3+mony
-            shuger=buyness
+            money=buyness*-3+money
+            sugar=buyness
     elif buy==4:
-        if mony<=buyness*-2+mony:
+        if money<=buyness*-2+money:
             print("your to poor")
         else:
-            mony=buyness*-1+mony
+            money=buyness*-1+money
             ice=buyness
-    print (lemon,cup,ice,shuger,mony)
-    return lemon,cup,ice,shuger,mony
+    print (lemon,cup,ice,sugar,money)
+    return lemon,cup,ice,sugar,money
+
 shop(600,0,0,0,0)
