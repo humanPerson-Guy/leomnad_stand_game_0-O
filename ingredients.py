6adfdaf211bf68eee5b7f660f5e9d60a73816202
humanPerson-Guy <ec541902@georgetownisd.org>
2026-01-30 09:39:57 -0600

    Refactor update

--- ingredients.py
+++ ingredients.py
@@ -1,41 +1,37 @@
-money=200
-lemon=0
-cup=0
-ice=0
-sugar=0
-
 def add(num,num1):
-    number= num+num1
-    return number
+        number= num+num1
+        return number
 
-buy=int(input("1 =lemon 2=cup 3=sugar 4=ice"))
-buyness=int(input("how much you whant to buy"))
-if buyness <= 0:
-    print("please put in what you want to buy.")
-elif buy==1:
-    if money<=buyness*-2+money:
-        print("you don't have enough.")
-    else:
-        money=buyness*-2+money
-        lemon=buyness
-elif buy==2:
-    if money<=buyness*-1+money:
-        print("you don't have enough.")
-    else:
-        money=buyness*-1+money
-        cupn=buyness
-elif buy==3:
-    if money<=buyness*-3+mony:
-        print("your to poor")
-    else:
-        money=buyness*-3+money
-        shuger=buyness
-elif buy==4:
-    if money<=buyness*-2+money:
-        print("you dont have enough!")
-    else:
-        money=buyness*-1+money
-        ice=buyness
-print (money)
+def shop(mony=100,lemon=10,cup=10,ice=10,shuger=10):
 
-
+    buy=int(input("1 =lemon 2=cup 3=shuger 4=ice"))
+    buyness=int(input("how much you whant to buy"))
+    if buyness <= 0:
+        print("you diry")
+    elif buy==1:
+        if mony<=buyness*-2+mony:
+            print("your to poor")
+        else:
+            mony=buyness*-2+mony
+            lemon=buyness
+    elif buy==2:
+        if mony<=buyness*-1+mony:
+            print("your to poor")
+        else:
+            mony=buyness*-1+mony
+            cup=buyness
+    elif buy==3:
+        if mony<=buyness*-3+mony:
+            print("your to poor")
+        else:
+            mony=buyness*-3+mony
+            shuger=buyness
+    elif buy==4:
+        if mony<=buyness*-2+mony:
+            print("your to poor")
+        else:
+            mony=buyness*-1+mony
+            ice=buyness
+    print (lemon,cup,ice,shuger,mony)
+    return lemon,cup,ice,shuger,mony
+shop(600,0,0,0,0)
---
+++ recipe.py
@@ -0,0 +1,19 @@
+from ingrtants import shop
+
+
+goodies=shop()
+
+print(goodies["mony"])
+
+
+chos=input("1=shop 2=change recipe 3=change price")
+
+
+print(goodies)
+
+respy={
+    "lemon":1,
+    "cup":1,
+    "ice":1,
+    "suger":1
+}
