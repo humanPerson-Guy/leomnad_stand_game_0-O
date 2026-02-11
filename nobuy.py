97c9a55ee764e36c27aba77e40cf0dc3700674de
Alice <as537332@georgetownisd.org>
2026-02-11 14:11:02 -0600

    Update nobuy.py

--- nobuy.py
+++ nobuy.py
@@ -1,17 +1,18 @@
 from custmer import custmer
 import random
 
-resipy={
+resipe={
     "lemon":1,
     "cup":1,
     "ice":1,
-    "suger":1
+    "sugar":1
 }
 def buyOrnobuy(guy,recipy):
     goodness={
-        "sweet":guy["sweet"]-recipy["suger"],
-        "lemon":guy["lemon"]-recipy["lemon"],
-        "ice":guy["ice"]-recipy["ice"],
+        "sweet":guy["sweet"]-recipe["sugar"],
+        "lemon":guy["lemon"]-recipe["lemon"],
+        "ice":guy["ice"]-recipe["ice"],
     }
     print(goodness)
-buyOrnobuy(1,resipy)
+
+buyOrnobuy(1,resipe)
