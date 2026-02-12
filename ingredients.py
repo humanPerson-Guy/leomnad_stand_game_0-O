d42c9c92add312d1eb173a2f0c12789eaf260e4c
humanPerson-Guy <ec541902@georgetownisd.org>
2026-02-12 09:18:03 -0600

    Update ingredients.py

--- ingredients.py
+++ ingredients.py
@@ -1,13 +1,13 @@
-from introduction import money_two
 
+#this completes the purchase
 def add(num,num1):
         number= num+num1
         return number
 #this sets the basis for the code
-def shop(money=money_two,lemon=10,cup=10,ice=10,sugar=10):
+def shop(money=100,lemon=10,cup=10,ice=10,sugar=10):
 #player starting stats
-    buy=int(input("What would you like to buy? 1 = lemon, 2 = cup, 3 = sugar, 4 = ice "))
-    buyness=int(input("how much do you want to buy ?"))
+    buy=int(input("1 =lemon 2=cup 3=sugar 4=ice"))
+    buyness=int(input("how much do you want to buy?"))
     if buyness <= 0:
         print("you didn't buy anything.")
     elif buy==1:
@@ -15,34 +15,34 @@ def shop(money=money_two,lemon=10,cup=10,ice=10,sugar=10):
             print("you don't have enough")
         else:
             money=buyness*-2+money
-            lemon=buyness
+            lemon+=buyness
     elif buy==2:
         if money<=buyness*-1+money:
             print("you made a purchase ")
         else:
             money=buyness*-1+money
-            cup=buyness
+            cup=+buyness
     elif buy==3:
         if money<=buyness*-3+money:
             print("you don't have enough")
         else:
             money=buyness*-3+money
-            sugar=buyness
+            sugar+=buyness
     elif buy==4:
         if money<=buyness*-2+money:
             print("you dont have enough")
         else:
             money=buyness*-1+money
-            ice=buyness
+            ice+=buyness
     print (lemon,cup,ice,sugar,money)
-    return {
+    goodies={
         "lemon":lemon,
         "cup":cup,
         "ice":ice,
         "sugar":sugar,
         "money":money
-        }
-
-# shop(600,0,0,0,0)
+    }
+    return goodies
+#shop(600,0,0,0,0)
 
 #this completes the purchase
