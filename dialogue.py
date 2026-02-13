d510a71fba951d82ff431924c5b143c7f9fe8224
thakid205 <cb559978@georgetownisd.org>
2026-02-13 09:46:37 -0600

    Dialogue changes

---
+++ dialogue.py
@@ -0,0 +1,5 @@
+
+charcter_a = {
+    "sad" : "my name is joseph im i dept really bad with a loan shark if i go bankrupt he will come and get me please help my business ",
+    "happy" : "hey my name is joseph i sell lemonade i have different respise pick you flavor"
+}
