99833c2b290927ec0c33b61d7dfc49c924b35de6
humanPerson-Guy <ec541902@georgetownisd.org>
2026-02-13 09:42:32 -0600

    made nobuy work and alowed ice to work not just suger

--- nobuy.py
+++ nobuy.py
@@ -8,18 +8,35 @@ recipe={
     "sugar":1
 }
 def buyOrnobuy(guy,recipe):
+    
+    
     goodness={
         "sweet":guy["sweetness"]-recipe["sugar"],
         "ice":guy["ice"]-recipe["ice"],
 
     }
+    print(guy)
     chance=["y","y","y"]
     while goodness["sweet"]!= 0:
         if goodness["sweet"]>0:
-            chance.append["n"]
-            sweet+=1
-
-
+            chance.append("n")
+            goodness["sweet"]-=1
+        if goodness["sweet"]<0:
+            chance.append("n")
+            goodness["sweet"]+=1
+        print(goodness["sweet"])
+    while goodness["ice"]!= 0:
+        if goodness["ice"]>0:
+            chance.append("n")
+            goodness["ice"]-=1
+        if goodness["ice"]<0:
+            chance.append("n")
+            goodness["ice"]+=1
+        print(goodness["ice"])
+    print(chance)    
+    
+        
+       
 
 
 
