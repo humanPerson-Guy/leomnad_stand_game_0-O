2827ae4548a2a19e5ea2fbc3a2c3cd399747f0bf
humanPerson-Guy <ec541902@georgetownisd.org>
2026-02-18 10:23:02 -0600

    fixededed ited
    OnO

--- introduction.py
+++ introduction.py
@@ -3,6 +3,8 @@
 
 def intro():
     name = input("Enter your name: ")
+    print("5 minets ago you took a lone your credet score was -12 so you had to look for alturnidive loners. so you turned /n to the Mafia.You needed 6 dolars to buy your cat medicin.You forgot about you lone and /n" \
+    "after 4 minets it rose from 6$ to 1001$.So  you made a lemonaid stand .the mafia is hunting you bribe them with good lemanaid to let you live longer")
     print("Hello", name ,"welcome to lemonade stand! In this game you will sell lemonade to customers. The goal is to not die from the mafia. The rules are very simple ",name," start off with a amount of money and buy ingredients and create a recipe for your customers. If you end up with no money you go bankrupt and the mafia gets you at the end of 7 days you need 1000$ to pay of your debt to the mafia good luck.")
 
     
@@ -10,23 +12,19 @@ def intro():
     if difficulty == "Easy" or "1":
         print("You have 600 dollars.")
         mony=600
-    elif difficulty == "Medium" or "2":
+    if difficulty == "Medium" or "2":
         print("You have 400 dollars.")
-        mony=400
-        hardness="good boy"
-    elif difficulty == "Hard" or "3":
+        mony=400    
+    if difficulty == "Hard" or "3":
         print("You have 200 dollars.")
-        mony=200
-        hardness="good boy"
-    elif difficulty == "Impossible" or "4":
+        mony=200    
+    if difficulty == "Impossible" or "4":
         print("You have 100 dollars")
-        mony=99.68
-        hardness="good boy"
+        mony=99.68    
     else:
         print("you have choisen death")
-        hardness="DETH"
-        return mony,name,hardness
-
+        mony=7.5   
+    return mony,name
 
 
 
--- run.py
+++ run.py
@@ -9,15 +9,18 @@ recipe={
     "ice":1,
     "sugar":1
 }
-money,name,hardness=intro()
-print(hardness)
-goodies=shop(money)
-ch=input("what do you do 1 shop 2 see yoour goodies 3 set your lemonad price 4 change the recipe")
-if ch== "1":
+money, name=intro()
+while True:
     goodies=shop(money)
-if ch== "2":
-    print (goodies)
-if ch== "3":
-   price=pricer()
-if ch== "4":
-    recipe=resipyChanger()
+    ch=input("what do you do 1 shop 2 see yoour goodies 3 set your lemonad price 4 change the recipe and any thing else to end day")
+    if ch== "1":
+        goodies=shop(goodies)
+    if ch== "2":
+        print (goodies)
+    if ch== "3":
+        price=pricer()
+    if ch== "4":
+        recipe=resipyChanger()
+    else:
+        print("custimers are aproching")
+        break
