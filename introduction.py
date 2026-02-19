e56a6a849d1e7d25195b2eddec549e80534dd2a0
humanPerson-Guy <ec541902@georgetownisd.org>
2026-02-19 09:57:25 -0600

    Update introduction.py

--- introduction.py
+++ introduction.py
@@ -4,7 +4,8 @@
 def intro():
     name = input("Enter your name: ")
     print("5 minets ago you took a lone your credet score was -12 so you had to look for alturnidive loners. so you turned /n to the Mafia.You needed 6 dolars to buy your cat medicin.You forgot about you lone and /n" \
-    "after 4 minets it rose from 6$ to 1001$.So  you made a lemonaid stand .the mafia is hunting you bribe them with good lemanaid to let you live longer")
+    "after 4 minets it rose from 6$ to 1001$.So  you made a lemonaid stand .the mafia is hunting you bribe them with good lemanaid to let you live longer.if by the end of 7 days you cant pay off your debt you becom a rat /n" \
+    )
     print("Hello", name ,"welcome to lemonade stand! In this game you will sell lemonade to customers. The goal is to not die from the mafia. The rules are very simple ",name," start off with a amount of money and buy ingredients and create a recipe for your customers. If you end up with no money you go bankrupt and the mafia gets you at the end of 7 days you need 1000$ to pay of your debt to the mafia good luck.")
 
     
@@ -21,10 +22,14 @@ def intro():
     if difficulty == "Impossible" or "4":
         print("You have 100 dollars")
         mony=99.68    
+    if difficulty == "ez":
+        print("you have nothing")
+        mony=0.2
     else:
         print("you have choisen death")
         mony=7.5   
     return mony,name
+
 
 
 
