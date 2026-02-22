469e1dddd5088b89bb2ebe1d8b548e5c2bd2e9db
humanPerson-Guy <ec541902@georgetownisd.org>
2026-02-22 08:03:13 -0600

    made game loop fixed a bug

--- run.py
+++ run.py
@@ -19,6 +19,7 @@ goodies={
     "sugar":2,
     "ice":2
 }
+day=1
 print("you need to buy suplys")
 
 goodies=shop(goodies)
@@ -33,28 +34,30 @@ else :
 price=1
 x=5
 print("you should probly change your price from 1$")
-while die=="n":
-    
-    ch=input("what do you do 1 shop 2 see yoour goodies 3 set your lemonad price 4 change the recipe and any thing else to end day")
-    if ch== "1":
-        goodies=shop(goodies)
-    elif ch== "2":
-        print (goodies)
-    elif ch== "3":
-        price=pricer()
-    elif ch== "4":
-        recipe=resipyChanger(goodies)
+while day!=8 and die=="n":
+    while True:
         
-    else:
-        if goodies["money"]<=0:
-            die="y"
-        break
-    lemonaids=lemonais_count(goodies)
-for i in range(1,random.randint(x,8)):
-    c=Customer()
-    tipe,buy=buyOrnobuy(c.get_customer_attributes(),recipe,price,lemonaids)
-    if buy=="y":
-        goodies["money"]+=price
-        lemonaids=-1
-        print(goodies["money"])
-    
+        ch=input("what do you do 1 shop 2 see yoour goodies 3 set your lemonad price 4 change the recipe and any thing else to end day")
+        if ch== "1":
+            goodies=shop(goodies)
+        elif ch== "2":
+            print (goodies)
+        elif ch== "3":
+            price=pricer()
+        elif ch== "4":
+            recipe=resipyChanger(goodies)
+            
+        else:
+            if goodies["money"]<=0:
+                die="y"
+            break
+        lemonaids=lemonais_count(goodies)
+    for i in range(1,random.randint(x,8)):
+        c=Customer()
+        tipe,buy=buyOrnobuy(c.get_customer_attributes(),recipe,price,lemonaids)
+        if buy=="y":
+            goodies["money"]+=price
+            lemonaids-=1
+            print(goodies["money"])
+    day+=1
+        
