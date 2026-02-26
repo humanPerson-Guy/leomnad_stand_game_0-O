e87da0c6db395aba00f55702abecea7c2f4edd89
Jose <jy537193@georgetownisd.org>
2026-02-26 09:47:20 -0600

    Update introduction.py

--- introduction.py
+++ introduction.py
@@ -9,26 +9,26 @@ def intro():
     print("Hello", name ,"welcome to lemonade stand! In this game you will sell lemonade to customers. The goal is to not die from the mafia. The rules are very simple",name,"start off with a amount of money and buy ingredients and create a recipe for your customers. If you end up with no money you go bankrupt and the mafia gets you at the end of 7 days you need 1000$ to pay of your debt to the mafia good luck.")
 
     
-    difficulty = input("Choose difficulty Easy/Medium/Hard/Impossible: ")
-    if difficulty == "Easy" or "1":
-        print("You have 600 dollars.")
-        mony = 600
-    elif difficulty == "Medium" or "2":
-        print("You have 400 dollars.")
-        mony = 400    
-    elif difficulty == "Hard" or "3":
-        print("You have 200 dollars.")
-        mony = 200    
-    elif difficulty == "Impossible" or "4":
-        print("You have 100 dollars")
-        mony = 99.68    
-    elif difficulty == "ez":
-        print("you have nothing")
-        mony=0.2
-        break
-    else:
-         print("invalid input")
-    return mony,name
+difficulty = input("Choose difficulty Easy/Medium/Hard/Impossible: ")
+    while True:
+        if difficulty == "Easy" or "1":
+            print("You have 600 dollars.")
+            mony = 600
+        elif difficulty == "Medium" or "2":
+            print("You have 400 dollars.")
+            mony = 400    
+        elif difficulty == "Hard" or "3":
+            print("You have 200 dollars.")
+            mony = 200    
+        elif difficulty == "Impossible" or "4":
+            print("You have 100 dollars")
+            mony = 99.68    
+        elif difficulty == "ez":
+            print("you have nothing")
+            mony=0.2
+        else:
+             print("invalid input")
+            return mony,name
 
 
 
