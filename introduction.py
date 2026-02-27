d2d4c250744997133ff451f593130eb75e28d161
Alice <as537332@georgetownisd.org>
2026-02-27 09:43:48 -0600

    Update introduction.py

--- introduction.py
+++ introduction.py
@@ -2,8 +2,8 @@
 
 
 def intro():
-    print("5 minutes ago you took a loan your credit score was -12 so you had to look for alternitive loaners. so you turned in to the Mafia. You needed 6 dollars to buy your cat medicine. You forgot about your loan and /n" \
-    "after 4 minutes it rose from 6$ to 1001$. So you made a lemonade stand as one of your part time jobs. The mafia is hunting you bribe them with good lemonade to let you live longer. If by the end of 7 days you cant pay off your debt you becom a rat"
+    print("Last week you needed to take a loan; your credit score was...low so you had to look for alternitive loaners.You had the bright idea to turn to the Lake Shore Mafia. You needed 600 dollars to buy your cat medicine. You forgot about your loan and /n" \
+    "after 4 minutes it rose from $600 to $1000; So you made a lemonade stand to pay off your debt. The mafia is hunting you. Bribe them with good lemonade to give you more time to pay off your loan. If by the end of 7 days you cant pay off your debt you may not be a very happy camper."
     )
     name = input("Enter your name: ")
     print("Hello", name ,"welcome to lemonade stand! In this game you will sell lemonade to customers. The goal is to not die from the mafia. The rules are very simple",name,"start off with a amount of money and buy ingredients and create a recipe for your customers. If you end up with no money you go bankrupt and the mafia gets you at the end of 7 days you need 1000$ to pay of your debt to the mafia good luck.")
@@ -53,6 +53,7 @@ difficulty = input("Choose difficulty Easy/Medium/Hard/Impossible: ")
              print("invalid input")
             return mony,name
 >>>>>>> 0407fca2208e98c5cd7430ba062bf87d2ea47a65
+
 
 
 
