e5521ea619bc1ac6c22f50708d3bc809c3df2604
humanPerson-Guy <ec541902@georgetownisd.org>
2026-03-02 09:46:16 -0600

    d
    fa

--- introduction.py
+++ introduction.py
@@ -6,29 +6,30 @@ def intro():
     "after 4 minutes it rose from 6$ to 1001$. So you made a lemonade stand as one of your part time jobs. The mafia is hunting you bribe them with good lemonade to let you live longer. If by the end of 7 days you cant pay off your debt you becom a rat"
     )
 
-name = input("Enter your name: ")
-print("Hello",name,"welcome to lemonade stand! In this game you will sell lemonade to customers. The goal is to not die from the mafia. The rules are very simple",name,"start off with a amount of money and buy ingredients and create a recipe for your customers. If you end up with no money you go bankrupt and the mafia gets you at the end of 7 days. you need 1000$ to pay of your debt. good luck!")
+    name = input("Enter your name: ")
+    print("Hello",name,"welcome to lemonade stand! In this game you will sell lemonade to customers. The goal is to not die from the mafia. The rules are very simple",name,"start off with a amount of money and buy ingredients and create a recipe for your customers. If you end up with no money you go bankrupt and the mafia gets you at the end of 7 days. you need 1000$ to pay of your debt. good luck!")
 
-difficulty = input("Choose difficulty Easy/Medium/Hard/Impossible: ")
-while True:
-        if difficulty == "Easy" or "1":
-            print("You have 600 dollars.")
-            money = 600
-        elif difficulty == "Medium" or "2":
-            print("You have 400 dollars.")
-            money = 400
-            break    
-        elif difficulty == "Hard" or "3":
-            print("You have 200 dollars.")
-            money = 200
-            break    
-        elif difficulty == "Impossible" or "4":
-            print("You have 100 dollars")
-            money = 99.68
-            break    
-        elif difficulty == "ez":
-            print("you have nothing")
-            money = 0.2
-            break
-        else:
-             print("invalid input")
+    difficulty = input("Choose difficulty Easy/Medium/Hard/Impossible: ")
+    while True:
+            if difficulty == "Easy" or "1":
+                print("You have 600 dollars.")
+                money = 600
+            elif difficulty == "Medium" or "2":
+                print("You have 400 dollars.")
+                money = 400
+                break    
+            elif difficulty == "Hard" or "3":
+                print("You have 200 dollars.")
+                money = 200
+                break    
+            elif difficulty == "Impossible" or "4":
+                print("You have 100 dollars")
+                money = 99.68
+                break    
+            elif difficulty == "ez":
+                print("you have nothing")
+                money = 0.2
+                break
+            else:
+                print("invalid input")            
+            return money,name
