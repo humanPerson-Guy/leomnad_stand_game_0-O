045eadc7d5231bf8d8915021751cc216979d5520
Jose <jy537193@georgetownisd.org>
2026-03-09 09:00:50 -0500

    Delete dialogue.py

--- dialogue.py
+++
@@ -1,5 +0,0 @@
-
-charcter_a = {
-    "sad" : "My name is Joseph, im in dept really bad with a loan shark! If i go bankrupt he will come and get me!!!! please help me reach my goal. ",
-    "happy" : "Hey my name is Joseph. I sell lemonade to pay for my cat's medication, but I can't pick a recipe! Create recipe:"
-}
